5a7293e8e8972fe848b08185f00df2b60f2c1f17
BobbyBytes <robbie234@gmail.com>
2020-03-10 23:09:31 -0400

    Added maps to SelectStore Screen

--- App1/src/screens/SetStoreScreen.js
+++ App1/src/screens/SetStoreScreen.js
@@ -1,11 +1,35 @@
-import React from 'react'
-import {View, Text, StyleSheet, FlatList } from 'react-native';
+import React from "react";
+import MapView from "react-native-maps";
+import { View, Text, StyleSheet, FlatList, Dimensions } from "react-native";
 
 const SetStoreScreen = () => {
-return <View>
-      <Text>SetStoreScreen</Text>
-  </View>
-
+  return (
+    <View>
+      <View style={styles.container}>
+        <MapView
+          style={styles.mapStyle}
+          initialRegion={{
+            latitude: 42.551884,
+            longitude: -71.103507,
+            latitudeDelta: 0.5622,
+            longitudeDelta: 0.3221
+          }}
+        ></MapView>
+      </View>
+    </View>
+  );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  mapStyle: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height
+  }
+});
 export default SetStoreScreen;
