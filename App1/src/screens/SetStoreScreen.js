64f0bef4029a017090d87298f86ffe6f35b63abb
BobbyBytes <robbie234@gmail.com>
2020-03-11 00:50:46 -0400

    Long press activates a location alert on the map

--- App1/src/screens/SetStoreScreen.js
+++ App1/src/screens/SetStoreScreen.js
@@ -14,12 +14,20 @@ const SetStoreScreen = () => {
             latitudeDelta: 0.5622,
             longitudeDelta: 0.3221
           }}
+          maptype={"hybrid"}
+          //onLongPress={(e) => alert("help")}
+          onLongPress={(e) => alert(JSON.stringify(e.nativeEvent.coordinate))}
+          provider={"google"}
         ></MapView>
       </View>
     </View>
   );
 };
 
+function onLongPress({ coordinate: LatLng, position: Point }) {
+  console.error("sddfdsf");
+  Alert.alert("Hey it's an updated message Robert");
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
