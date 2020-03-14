32b8041d4928a783fc4a8ee8351a9f82285e0c58
BobbyBytes <robbie234@gmail.com>
2020-03-14 12:00:31 -0400

    Can now long press to save location. Location is stored locally (persistant) on device. Button to load test data added.

--- App1/src/screens/SetStoreScreen.js
+++ App1/src/screens/SetStoreScreen.js
@@ -1,10 +1,11 @@
 import React from "react";
 import MapView from "react-native-maps";
-import { View, Text, StyleSheet, FlatList, Dimensions } from "react-native";
+import { View, Text, StyleSheet, FlatList, Dimensions, AsyncStorage, Button } from "react-native";
+
 
 const SetStoreScreen = () => {
   return (
-    <View>
+    <View style={styles.myFlexContainerColumn}>
       <View style={styles.container}>
         <MapView
           style={styles.mapStyle}
@@ -14,23 +15,57 @@ const SetStoreScreen = () => {
             latitudeDelta: 0.5622,
             longitudeDelta: 0.3221
           }}
-          maptype={"hybrid"}
-          //onLongPress={(e) => alert("help")}
-          onLongPress={(e) => alert(JSON.stringify(e.nativeEvent.coordinate))}
-          provider={"google"}
+          maptype={"hybrid"} //Allows points of interest on map
+          onLongPress={(e) => _storeData(JSON.stringify(e.nativeEvent.coordinate))}
+          provider={"google"} //Force google maps for now
         ></MapView>
+      </View>
+      <View style={styles.buttonStyle}>
+        <Button
+        title = "StoreData"
+        backgroundColor = "white"
+        onPress = {() => _storeData()}
+        height = "100"
+        ></Button>
+        <Button
+        title = "LoadData"
+        backgroundColor = "white"
+        onPress = {() => _retrieveData()}
+        height = "100"
+        ></Button>
       </View>
     </View>
   );
 };
-
-function onLongPress({ coordinate: LatLng, position: Point }) {
-  console.error("sddfdsf");
-  Alert.alert("Hey it's an updated message Robert");
+//Store Persistant Data function
+_storeData = async (coordinate) => {
+  try {
+    await AsyncStorage.setItem('TASKS', coordinate);
+    alert("Location Stored")
+  } catch (error) {
+    // Error saving data
+  }
+};
+//Retrieve Persistant data function
+_retrieveData = async () => {
+  try {
+    const value = await AsyncStorage.getItem('TASKS');
+    if (value !== null) {
+      // We have data!!
+      alert("Saved Loactation: \n\n" + value);
+    }
+  } catch (error) {
+    // Error retrieving data
+  }
+};
+//Test function
+function printCoordinate(coordinate){
+  //Print test data
+  alert(coordinate);
 }
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
@@ -38,6 +73,18 @@ const styles = StyleSheet.create({
   mapStyle: {
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height
-  }
+  },
+myFlexContainerColumn: {
+  flex: 1,
+  flexDirection: 'column',
+  justifyContent: 'center'
+},
+buttonStyle: {
+  flex: 1,
+  backgroundColor: "#fff",
+  alignItems: "center",
+  height: 1,
+  justifyContent: "center"
+}
 });
 export default SetStoreScreen;
