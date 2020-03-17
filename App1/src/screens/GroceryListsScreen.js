3544f61bd09ab65bfe4b0c48ee7f1682c1c7a12c
jjjoycing123 <john.nay220@gmail.com>
2020-03-17 18:06:15 -0400

    Created ImageDetails.js. Added FlatList to GroceryListScreen.js

--- App1/src/screens/GroceryListsScreen.js
+++ App1/src/screens/GroceryListsScreen.js
@@ -3,17 +3,47 @@ import {View, Text, StyleSheet, FlatList, Button} from 'react-native';
 
 const GroceryListsScreen = ({navigation}) => {
 
-  return <View style={styles.container}>
-    <View style={styles.buttonContainer}>
+const lists = [
+{name: 'List 1'},
+{name: 'List 2'},
+{name: 'List 3'},
+{name: 'List 4'},
+{name: 'List 5'},
+{name: 'List 6'},
+{name: 'List 7'},
+{name: 'List 8'},
+{name: 'List 9'},
+{name: 'List 10'},
+
+];
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        vertical
+        showsVerticalScrollIndicator = {false}
+        data = {lists}
+        keyExtractor={list => list.name}
+        renderItem={({ item }) => {
+          return (
+            <Text style= {styles.textStyle}>{item.name}</Text>
+          );
+        }}
+        />
+      <View style={styles.buttonContainer}>
       <Button
           onPress = {() => navigation.navigate("GroceryList")}
           title = "Make List"
           />
           </View>
+
+
         </View>
+    );
 };
 
 const styles = StyleSheet.create({
+
 container: {
   flex: 1,
   justifyContent: 'center',
@@ -21,6 +51,9 @@ container: {
   buttonContainer: {
   margin: 30
   },
+  textStyle: {
+  marginVertical: 30
+}
 });
 
 export default GroceryListsScreen;
