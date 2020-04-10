5dc3112e9be90d7599c21f5b354a69909c2fb81f
jjjoycing123 <john.nay220@gmail.com>
2020-04-10 11:18:04 -0400

    Updated GroceryList File

--- App1/src/screens/GroceryListScreen.js
+++ App1/src/screens/GroceryListScreen.js
@@ -1,89 +1,106 @@
-import React, { useReducer } from 'react'
+
+import React, { useReducer, useState } from 'react'
 import {View, Text, StyleSheet, FlatList, Button, TouchableOpacity, Alert } from 'react-native';
 
 const reducer = (state, action) => {
-  // state === {count: number }
-  // action === {type: 'increment', || 'decrement', payload: 1}
-  switch (action.type) {
-    case 'increment':
-      return { ...state, count: state.count + action.payload };
-    case 'decrement':
-      return { ...state, count: state.count - action.payload };
-    default:
-      return state;
-  }
+ // state === {count: number }
+ // action === {type: 'increment', || 'decrement', payload: 1}
+ switch (action.type) {
+   case 'increment':
+     return { ...state, count: state.count + action.payload };
+   case 'decrement':
+     return { ...state, count: state.count - action.payload };
+   default:
+     return state;
+ }
 };
 
 const GroceryListScreen = ({navigation}) => {
-  const[state, dispatch] = useReducer(reducer, {count: 0})
-  const food_items = [
-  {name: 'eggs:'},
-  {name: 'milk:'},
-  {name: 'bread:'},
-  {name: 'coffee:'},
-  {name: 'beer:'},
-  {name: 'cheese:'},
-  {name: 'broccoli:'},
-  {name: 'rice:'},
-  {name: 'carrot:'},
+ const[state, dispatch] = useReducer(reducer, {count: 0})
+ const food_items = [
+ {name: 'eggs:'},
+ {name: 'milk:'},
+ {name: 'bread:'},
+ {name: 'coffee:'},
+ {name: 'beer:'},
+ {name: 'cheese:'},
+ {name: 'broccoli:'},
+ {name: 'rice:'},
+ {name: 'carrot:'},
 ];
+const [grocery_items, set_grocery_items] = useState([])
+console.log(grocery_items);
 
+//Helper function
 const setCounter_increment = (item) => {
   dispatch({type: 'increment', payload: 1})
   Alert.alert(`${item.name} pressed`)
+  set_grocery_items([...grocery_items, item.name]);
 }
-  return (
-    <View style={styles.container}>
-      <FlatList
-        vertical
-        showsVerticalScrollIndicator = {false}
-        data = {food_items}
-        keyExtractor={food_item => food_item.name}
-        renderItem={({ item }) => {
-            return (
-                <View style={styles.container}>
-                    <Text style= {styles.textStyle}>{item.name}
-                      </Text>
-                  <View style={styles.buttonContainer_one}>
-                    <Button onPress ={console.log("FlatList button QTY pressed")}
-                    title = "QTY Increase:"/>
-                    </View>
-                    <View style={styles.buttonContainer_one}>
-                      <Button onPress ={console.log("FlatList button QTY pressed")}
-                      title = "QTY Decrease:"/>
-                      </View>
-                </View>
-            );
-        }} />
 
-      <View style={styles.buttonContainer_two}>
-        <Button
-            onPress = {() => navigation.navigate("Reminder")}
-            title = "Set Reminder"
-            />
-            </View>
-              <Text> Current Count: {state.count} </Text>
-        </View>
-      );
+ return (
+   <View style={styles.container_one}>
+     <FlatList
+       vertical
+       showsVerticalScrollIndicator = {false}
+       data = {food_items}
+       keyExtractor={food_item => food_item.name}
+       renderItem={({ item }) => {
+           return (
+             <TouchableOpacity
+                onPress= {() => setCounter_increment(item)}
+                >
+                <Text style= {styles.textStyle}>{item.name}</Text>
+               </TouchableOpacity>
+           );
+       }} />
+
+         <FlatList
+          keyExtractor={(item) => item}
+           vertical
+           showsVerticalScrollIndicator = {false}
+           data = {grocery_items}
+           renderItem={({ item }) => {
+               return (
+                    <Text style= {styles.textStyle_two}>{item}</Text>
+               );
+           }}
+           />
+
+
+     <View style={styles.buttonContainer_two}>
+       <Button
+           onPress = {() => navigation.navigate("Reminder")}
+           title = "Set Reminder"
+           />
+           </View>
+             <Text> Current Count: {state.count} </Text>
+       </View>
+     );
 };
 
 const styles = StyleSheet.create({
-container: {
-  flex: 1,
-  justifyContent: 'center',
+container_one: {
+ flex: 1,
+ justifyContent: 'center',
 },
 buttonContainer_one: {
-  marginRight: 250,
-  margin: 20,
-  justifyContent: 'flex-start'
+ marginRight: 250,
+ margin: 20,
+ justifyContent: 'flex-start'
 },
 buttonContainer_two: {
-  margin: 30
-  },
+ margin: 30
+ },
 textStyle: {
-  fontSize: 15,
-  //marginVertical: 30
+ fontSize: 15,
+ marginVertical: 30
+},
+textStyle_two: {
+ backgroundColor: "green",
+ fontSize: 15,
 }
+
 });
 
 export default GroceryListScreen;
